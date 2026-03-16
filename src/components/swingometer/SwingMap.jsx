7f085ce806c4c71278c2e43416231c5d45c1613c
Maryland Predictions <marylandpredictions@gmail.com>
2026-03-16 02:07:33 +0000

    File changes

--- src/components/swingometer/SwingMap.jsx
+++ src/components/swingometer/SwingMap.jsx
@@ -82,8 +82,8 @@ export default function SwingMap({ baseResults, swing }) {
   const stateEntries = Object.entries(statePositions);
   
   // Calculate seat counts
-  let demSeats = 32;
-  let repSeats = 31;
+  let demSeats = 33;
+  let repSeats = 32;
   let tossUpSeats = 0;
   
   Object.entries(baseResults).forEach(([state, baseMargin]) => {
