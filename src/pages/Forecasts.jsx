8c81932ae1e4f352d0265359c1c4dec814bde942
Maryland Predictions <marylandpredictions@gmail.com>
2026-03-17 05:45:44 +0000

    File changes

--- src/pages/Forecasts.jsx
+++ src/pages/Forecasts.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import InteractiveMap from '../components/forecasts/InteractiveMap';
 import SenateControl from '../components/forecasts/SenateControl';
